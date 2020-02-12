5d8be0fa9910c2f30bbdeaf69e680ff24afd4a0c
edvargas <v-edvargas@expedia.com>
2020-02-11 22:17:56 -0600

    ejercicio 1 de lista del super

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [lista, setLista] = useState(['leche', 'huevos', 'pan']);
+  const [nuevoItem, setNuevoItem] = useState();
+
+  const guardarItem = evento => {
+    setNuevoItem(evento.target.value);
+  };
+
+  const agregarItem = () => {
+    const nuevaLista = lista;
+    nuevaLista.push(nuevoItem);
+    setLista(nuevaLista);
+  };
+
   return (
     <div className="App">
-      <p>hello world</p>
+      <h1>Lista</h1>
+      <ul>
+        {
+          lista.map((item) => {
+            return (
+              <li>
+                {item}
+              </li>
+            )
+          })
+        }
+      </ul>
+      <div>
+        <input
+          type="text"
+          placeholder="nuevo elemento"
+          onChange={guardarItem} />
+        <button onClick={agregarItem}>Agregar</button>
+      </div>
     </div>
   );
 }
